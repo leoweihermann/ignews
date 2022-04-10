19264600fd5cbb87bb6ff9a7d60e5e0c6b97d8dd
Leonardo Weihermann <leonardoweihermann@gmail.com>
2022-04-10 15:15:56 -0300

    feat: add more action to stripe events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -20,7 +20,11 @@ export const config = {
   },
 };
 
-const relevantEvents = new Set(["checkout.session.completed"]);
+const relevantEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
@@ -50,7 +54,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
               .object as Stripe.Checkout.Session;
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
+            );
+            break;
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+            const subscription = event.data.object as Stripe.Subscription;
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString()
             );
             break;
           default:
